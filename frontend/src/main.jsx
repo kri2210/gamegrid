4fc3f30e79f54c41d36e13c772e03ce2bb6cbc2a
SHAH krish mehul <163560709+kri2210@users.noreply.github.com>
2026-04-18 15:28:51 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import axios from 'axios';
 import App from './App.jsx';
 import './index.css';
-
-axios.defaults.baseURL = 'https://gamegrid-backend-i9yk.onrender.com';
-axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
