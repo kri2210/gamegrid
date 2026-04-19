69403125ee04735de6d64c434d4ad0c286b19c86
SHAH krish mehul <163560709+kri2210@users.noreply.github.com>
2026-04-19 09:17:28 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://gamegrid-backend-i9yk.onrender.com',
+        target: 'http://localhost:5000',
         changeOrigin: true,
       }
     }
